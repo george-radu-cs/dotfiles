1e2e353a150ad5421760cccba479079d450b4b6c
George Radu <radu.george.mihai.21@gmail.com>
2023-08-25 02:02:25 +0300

    fix kube file and add btop

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install vim neovim neofetch zsh evince zathura cava htop kitty ranger xournal telegram-desktop vlc timeshift steam firmware-misc-nonfree rkhunter lynis debsecan debsums needrestart clamav
+sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install vim neovim neofetch zsh evince zathura cava btop kitty ranger xournal telegram-desktop vlc timeshift firmware-misc-nonfree rkhunter lynis debsecan debsums needrestart clamav
