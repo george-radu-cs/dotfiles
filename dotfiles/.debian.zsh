9671558a766cd13b006726ddbd7a6617f58cff06
George Radu <radu.george.mihai.21@gmail.com>
2023-08-26 02:27:53 +0300

    fix: debian go and java path

--- dotfiles/.debian.zsh
+++ dotfiles/.debian.zsh
@@ -10,18 +10,18 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 
 # go
-export PATH=$PATH:${HOME}/go/bin/
-export GOPATH="${HOME}/go/bin/"
+export PATH=$PATH:/usr/local/go/bin/
+export GOPATH="/usr/local/go/bin/"
 
 # flutter
 export PATH=$PATH:${HOME}/development/flutter/bin
 
 # java with aliases
-export JAVA_HOME=$(/usr/libexec/java_home)
-export JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8)
-export JAVA_11_HOME=$(/usr/libexec/java_home -v 11)
-export JAVA_17_HOME=$(/usr/libexec/java_home -v 17)
-export JAVA_20_HOME=$(/usr/libexec/java_home -v 20)
+export JAVA_HOME=/usr/lib/jvm/temurin-20-jdk-amd64
+export JAVA_8_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
+export JAVA_11_HOME=/usr/lib/jvm/temurin-11-jdk-amd64
+export JAVA_17_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
+export JAVA_20_HOME=/usr/lib/jvm/temurin-20-jdk-amd64
 
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
 alias java11='export JAVA_HOME=$JAVA_11_HOME'
